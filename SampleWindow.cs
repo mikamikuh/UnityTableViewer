b41748f2684a18630154dba59acab1661703ecb1
mikamikuh <mkouhei0910@gmail.com>
2012-07-29 00:44:25 +0900

    Change SampleWindow for use to function of AccessorManager generation

--- SampleWindow.cs
+++ SampleWindow.cs
@@ -2,24 +2,37 @@ using System;
 using System.Threading;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 using System.Collections;
+using System.Collections.Generic;
 using TemplateEngine;
+using UnityTableViewer.Generator;
 
 public class SampleWindow : EditorWindow {
 	private Boolean flag = false;
     
-    [MenuItem ("Window/My Window %g")]
+    [MenuItem ("Window/Sample Window %g")]
     static void Init () {
-         MyWindow window = (MyWindow)EditorWindow.GetWindow (typeof (MyWindow));
+         SampleWindow window = (SampleWindow)EditorWindow.GetWindow (typeof (SampleWindow));
     }
     
     void OnGUI () {
+		if(GUILayout.Button("Generate AccessorManager")) {
+			IList<string> prefabNames = new List<string>();
+			DirectoryInfo info = new DirectoryInfo("Assets/SourcePrefab/");
+			foreach(FileInfo f in info.GetFiles()) {
+				prefabNames.Add (f.Name.Split('.')[0]);
+			}
+			
+			//UnityEngine.Object[] objects = AssetDatabase.LoadAllAssetsAtPath("Assets/SourcePrefab/");
+			//UnityEngine.Object obj = AssetDatabase.LoadAssetAtPath("Assets/SourcePrefab/Cube", typeof(GameObject));
+			//Debug.Log (obj.name);
+			//IList<UnityEngine.Object> prefabs = new List<UnityEngine.Object>(objects);
+			ICodeGenerator generator = new AccessorManagerGenerator("Test.Generate", "Sample", prefabNames);
+			generator.execute();
+		}
+		
 		if(GUILayout.Button("Click")){
-			Template target = new Template("Assets/Templates/csharp.txt", false);
-			target.Set("className", "GenerateClassName");
-			target.Set("extendClassName", "MonoBehaviour");
-			System.IO.File.WriteAllText("Assets/GenerateClassName.cs", target.ToString());
-			Debug.Log (target.ToString());
 			AssetDatabase.ImportAsset("Assets/GenerateClassName.cs");
 			AssetDatabase.Refresh();
 			flag = true;
