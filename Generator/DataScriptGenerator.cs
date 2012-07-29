af48b037c03135786da43def08ce5235f7b0b6d8
mikamikuh <mkouhei0910@gmail.com>
2012-07-29 11:06:48 +0900

    Implement function of DataScriptGenerator

---
+++ Generator/DataScriptGenerator.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityTableViewer.Utility;
+using TemplateEngine;
+
+namespace UnityTableViewer.Generator {
+	public class DataScriptGenerator : CodeGenerator {
+		
+		private string ns;
+		private string className;
+		private IDictionary<string, string> variables;
+		
+		public DataScriptGenerator(string ns, string className, IDictionary<string, string> variables) {
+			this.ns = ns;
+			this.className = className;
+			this.variables = variables;
+		}
+		
+		public override void execute() {
+			Template target = new Template(AssetPathUtility.DataScriptTemplatePath, false);
+			target.Set("className", className);
+			target.Set("namespace", ns);
+			
+			string variableCode = "";
+			
+			foreach(KeyValuePair<string, string> pair in variables) {
+				string line = "		public " + pair.Value + " " + pair.Key + ";";
+				variableCode += line + System.Environment.NewLine;
+			}
+			
+			target.Set("variables", variableCode);
+			
+			string folderPath = AssetPathUtility.DataScriptGeneratePath;
+			if(!System.IO.Directory.Exists(folderPath)) {
+				System.IO.Directory.CreateDirectory(folderPath);
+			}
+			
+			System.IO.File.WriteAllText(NamingRuleUtility.CreateDataScriptGeneratePath(className), target.ToString());
+			AssetDatabase.Refresh();
+		}
+	}
+}
