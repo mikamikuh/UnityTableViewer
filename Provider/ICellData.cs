0a311e144dfd46eb18e9883b8835bceb02ec8941
mikamikuh <mkouhei0910@gmail.com>
2012-08-02 03:05:03 +0900

    Create variable Data. Delete variable Get and Set

--- Provider/ICellData.cs
+++ Provider/ICellData.cs
@@ -1,10 +1,10 @@
+using System;
 using UnityEngine;
 using System.Collections;
 
 namespace UnityTableViewer.Provider {
 	public interface ICellData {
 		string Name { get; }
-		string Get { get; }
-		string Set { set; }
+		System.Object Data { get; set; }
 	}
 }
