629ee50362e920d5274a17b6f886ebdaaaac720c
mikamikuh <mkouhei0910@gmail.com>
2012-08-08 04:21:21 +0900

    Add name setter method to ICellProvider

--- Provider/ICellData.cs
+++ Provider/ICellData.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 namespace UnityTableViewer.Provider {
 	public interface ICellData {
-		string Name { get; }
+		string Name { get; set; }
 		System.Object GetData(string name);
 		void SetData(System.Object obj, string name);
 	}
